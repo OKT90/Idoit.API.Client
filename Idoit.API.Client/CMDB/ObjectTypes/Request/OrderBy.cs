7bb89c18874c34b637fab5e667c60afc21867e2f
unknown <otalaat@i-doit.com>
2021-02-03 14:20:07 +0100

    Added a new class with the name 'OrderBy' to be able to order by the title for example

---
+++ Idoit.API.Client/CMDB/ObjectTypes/Request/OrderBy.cs
@@ -0,0 +1,9 @@
+﻿namespace Idoit.API.Client.CMDB.ObjectTypes.Request
+{
+    public class OrderBy
+    {
+        public const string Title = "title";
+        public const string Id = "id";
+        public const string Status = "status";
+    }
+}
