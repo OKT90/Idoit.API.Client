40a9966618ad95c5cfba545f0a4670a8de4c121b
unknown <otalaat@i-doit.com>
2021-03-17 11:23:18 +0100

    Created a new class with the name 'ObjectTypeCategories'

---
+++ Idoit.API.Client/CMDB/ObjectTypeCategories/ObjectTypeCategories.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using Result = Idoit.API.Client.CMDB.ObjectTypeCategories.Response.Result;
+
+namespace Idoit.API.Client.CMDB.ObjectTypeCategories
+{
+    public class ObjectTypeCategories
+    {
+        public ObjectTypeCategories(Client myClient)
+        {
+            client = myClient;
+        }
+        Dictionary<string, object> parameter;
+        Result response;
+        public Client client;
+        public Result Read(string type)
+        {
+            Task t = Task.Run(() => { Reading(type).Wait(); }); t.Wait();
+            return response;
+        }
+
+        async Task Reading(string type)
+        {
+            parameter = client.GetParameter();
+            parameter.Add("type", type);
+            response = await client.GetConnection().InvokeAsync<Result>("cmdb.object_type_categories.read", parameter);
+        }
+    }
+}
