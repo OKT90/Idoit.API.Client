3b9ee818ac67fdfe31cc71b1d0911b5e7c5de1e1
unknown <otalaat@i-doit.com>
2021-03-19 14:16:48 +0100

    Added a new class with the name 'Date' to the name space 'Attribute'

---
+++ Idoit.API.Client/CMDB/Category/Response/Attribute/Date.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Idoit.API.Client.CMDB.Category.Response.Attribute
+{
+    public class Date
+    {
+        [JsonProperty("title")]
+        public string date { get; set; }
+        [JsonProperty("prop_type")]
+        public string typ { get; set; }
+    }
+}
