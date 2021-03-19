9058b74c7403b9e08ddc3d88695c26ce5fcb48a1
unknown <otalaat@i-doit.com>
2021-03-19 14:17:23 +0100

    Added a new class with the name 'AssignedContact' to the name space 'Attribute'

---
+++ Idoit.API.Client/CMDB/Category/Response/Attribute/AssignedContact.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Idoit.API.Client.CMDB.Category.Response.Attribute
+{
+    public class AssignedContact : Attribute
+    {
+        [JsonProperty("sysid")]
+        public string sysid { get; set; }
+        [JsonProperty("type_title")]
+        public string typeTitle { get; set; }
+        [JsonProperty("prop_type")]
+        public string propType { get; set; }
+        [JsonProperty("identifier")]
+        public string identifier { get; set; }
+
+    }
+}
