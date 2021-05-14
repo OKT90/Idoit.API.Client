a89374a94a444fa64407d82ec43b67238bc1c0b1
unknown <otalaat@i-doit.com>
2021-05-14 13:08:07 +0200

    Changed the variable 'sysid' to 'sysID'

--- Idoit.API.Client/CMDB/Category/Response/Attribute/AssignedContact.cs
+++ Idoit.API.Client/CMDB/Category/Response/Attribute/AssignedContact.cs
@@ -5,13 +5,12 @@ namespace Idoit.API.Client.CMDB.Category.Response.Attribute
     public class AssignedContact : Attribute
     {
         [JsonProperty("sysid")]
-        public string sysid { get; set; }
+        public string sysID { get; set; }
         [JsonProperty("type_title")]
         public string typeTitle { get; set; }
         [JsonProperty("prop_type")]
         public string propType { get; set; }
         [JsonProperty("identifier")]
         public string identifier { get; set; }
-
     }
 }
