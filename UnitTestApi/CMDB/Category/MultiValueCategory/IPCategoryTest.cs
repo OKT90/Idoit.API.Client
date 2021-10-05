21c893244af34ff8ee80177e89b496dcea287fd1
Omar Talaat <otalaat@i-doit.com>
2021-10-05 14:49:17 +0200

    Added a unit test of the category 'IP'

--- UnitTestApi/CMDB/Category/MultiValueCategory/IPCategoryTest.cs
+++ UnitTestApi/CMDB/Category/MultiValueCategory/IPCategoryTest.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using Idoit.API.Client;
-using Idoit.API.Client.CMDB.Object.Response;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IP = Idoit.API.Client.CMDB.Category.IP;
 using IPRequset = Idoit.API.Client.CMDB.Category.Request.IP;
 using IPResponse = Idoit.API.Client.CMDB.Category.Response.IP;
-using IResponse = Idoit.API.Client.CMDB.Category.Response.IResponse;
 using Obj = Idoit.API.Client.CMDB.Object.Object;
 using ObjectType = Idoit.API.Client.Contants.ObjectTypes;
 using CmdbStatus = Idoit.API.Client.Contants.CmdbStatus;
@@ -29,7 +27,6 @@ namespace UnitTestApi.CMDB.Category.MultiValueCategory
             LANGUAGE = DotNetEnv.Env.GetString("LANGUAGE");
         }
 
-
         //Create
         [TestMethod]
         public void CreateTest()
@@ -44,14 +41,18 @@ namespace UnitTestApi.CMDB.Category.MultiValueCategory
             Obj objectRequest = new Obj(myClient);
             IP IP = new IP(myClient);
             //Act:Create the Object
-            objectRequest.type = ObjectType.CLUSTER;
-            objectRequest.title = " My Cluster";
+            objectRequest.type = ObjectType.SERVER;
+            objectRequest.title = " My Server";
             objectRequest.cmdbStatus = CmdbStatus.INOPERATION;
             objectId = objectRequest.Create();
 
             //Act: Create the Category
-            categoryRequest.ipv4_address = "1.1.1.2";
-            categoryRequest.description = "Web GUI description";
+            categoryRequest.ipv4_address = "0.0.0.100";
+            categoryRequest.primary = 0;
+            categoryRequest.active = 0;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.description = "IP category test description";
             cateId = IP.Create(objectId, categoryRequest);
 
             //Assert
@@ -61,13 +62,13 @@ namespace UnitTestApi.CMDB.Category.MultiValueCategory
             categoryRequest.category_id = cateId;
             list = IP.Read(objectId);
 
-
             //Assert
             foreach (IPResponse[] row in list)
             {
                 foreach (IPResponse element in row)
                 {
-                    Assert.IsNotNull(element.ipv4_address);
+                    Assert.IsNotNull(element.iPV4Address);
+                    Assert.AreEqual(element.active.title,"No");
                     Assert.IsNotNull(element.id);
                 }
             }
@@ -76,166 +77,148 @@ namespace UnitTestApi.CMDB.Category.MultiValueCategory
             objectRequest.Delete(objectId);
         }
 
-        ////Delete
-        //[TestMethod]
-        //public void DeleteTest()
-        //{
-        //    //Arrange
-        //    int cateId, objectId;
-        //    List<IPResponse[]> list = new List<IPResponse[]>();
-        //    Client myClient = new Client(URL, APIKEY, LANGUAGE);
-        //    myClient.Username = "admin";
-        //    myClient.Password = "admin";
-        //    Obj objectRequest = new Obj(myClient);
-        //    IPRequset categoryRequest = new IPRequset();
-        //    IP IP = new IP(myClient);
-
-        //    //Act:Create the Object
-        //    objectRequest.type = ObjectType.Client;
-        //    objectRequest.title = " My Client";
-        //    objectRequest.cmdbStatus = CmdbStatus.inOperation;
-        //    objectId = objectRequest.Create();
-
-        //    //Act: Create the Category
-        //    categoryRequest.title = "Web GUI";
-        //    categoryRequest.description = "Web GUI description";
-        //    categoryRequest.type = " ES";
-        //    categoryRequest.formatted_url = "https://swsan.admin.acme-it.example/";
-        //    categoryRequest.primary = "3";
-        //    cateId = IP.Create(objectId, categoryRequest);
-
-        //    //Act
-        //    IP.Delete(objectId, cateId);
-
-        //    //Act:Read the Category
-        //    categoryRequest.category_id = cateId;
-        //    list = IP.Read(objectId);
-
-        //    //Assert
-        //    foreach (IPResponse[] row in list)
-        //    {
-        //        foreach (IPResponse element in row)
-        //        {
-        //            Assert.IsNotNull(element.title);
-        //            Assert.IsNotNull(element.id);
-        //        }
-        //    }
-
-        //}
-
-        ////Quickpurge
-        //[TestMethod]
-        //public void QuickpurgeTest()
-        //{
-
-        //    //Arrange
-        //    int cateId, objectId;
-        //    Client myClient = new Client(URL, APIKEY, LANGUAGE);
-        //    myClient.Username = "admin";
-        //    myClient.Password = "admin";
-        //    Obj objectRequest = new Obj(myClient);
-        //    IPRequset categoryRequest = new IPRequset();
-        //    IP IP = new IP(myClient);
-
-        //    //Act:Create the Object
-        //    objectRequest.type = ObjectType.Client;
-        //    objectRequest.title = " My Client";
-        //    objectRequest.cmdbStatus = CmdbStatus.inOperation;
-        //    objectId = objectRequest.Create();
-
-        //    //Act: Create the Category
-        //    categoryRequest.title = "Web GUI";
-        //    categoryRequest.description = "Web GUI description";
-        //    categoryRequest.type = " ES";
-        //    categoryRequest.formatted_url = "https://swsan.admin.acme-it.example/";
-        //    categoryRequest.primary = "3";
-        //    cateId = IP.Create(objectId, categoryRequest);
-
-        //    //Act
-        //    IP.Quickpurge(objectId, cateId);
-        //}
-
-        ////Update
-        //[TestMethod]
-        //public void UpdateTest()
-        //{
-        //    //Arrange
-        //    int cateId, objectId;
-        //    List<IPResponse[]> list = new List<IPResponse[]>();
-        //    Client myClient = new Client(URL, APIKEY, LANGUAGE);
-        //    myClient.Username = "admin";
-        //    myClient.Password = "admin";
-        //    Obj objectRequest = new Obj(myClient);
-        //    IPRequset categoryRequest = new IPRequset();
-        //    IP IP = new IP(myClient);
-
-        //    //Act:Create the Object
-        //    objectRequest.type = ObjectType.Client;
-        //    objectRequest.title = " My Client";
-        //    objectRequest.cmdbStatus = CmdbStatus.inOperation;
-        //    objectId = objectRequest.Create();
-
-        //    //Act: Create the Category
-        //    categoryRequest.title = "Web GUI";
-        //    categoryRequest.description = "Web GUI description";
-        //    categoryRequest.type = " ES";
-        //    categoryRequest.formatted_url = "https://swsan.admin.acme-it.example/";
-        //    categoryRequest.primary = "3";
-        //    cateId = IP.Create(objectId, categoryRequest);
-
-        //    //Act: Update the Category
-        //    categoryRequest.title = "Web GUI 2";
-        //    categoryRequest.description = "Web GUI 2 description";
-        //    categoryRequest.type = " SE";
-        //    categoryRequest.formatted_url = "https://swsan.admin.acme-it.example/";
-        //    categoryRequest.primary = "4";
-        //    categoryRequest.category_id = cateId;
-        //    IP.Update(objectId, categoryRequest);
-
-        //    //Act:Read the Category
-        //    categoryRequest.category_id = cateId;
-        //    list = IP.Read(objectId);
-
-        //    //Assert
-        //    foreach (IPResponse[] row in list)
-        //    {
-        //        foreach (IPResponse element in row)
-        //        {
-        //            Assert.AreEqual("Web GUI 2", categoryRequest.title);
-        //        }
-        //    }
-
-        //    //Act:Delete the Object
-        //    objectRequest.Delete(objectId);
-
-        //}
-
-        //Read
+        //Delete
         [TestMethod]
-        public void ReadTest()
+        public void DeleteTest()
         {
             //Arrange
             int cateId, objectId;
             List<IPResponse[]> list = new List<IPResponse[]>();
+            IPRequset categoryRequest = new IPRequset();
             Client myClient = new Client(URL, APIKEY, LANGUAGE);
             myClient.Username = "admin";
             myClient.Password = "admin";
             Obj objectRequest = new Obj(myClient);
-            IPRequset categoryRequest = new IPRequset();
             IP IP = new IP(myClient);
-
             //Act:Create the Object
-            objectRequest.type = ObjectType.CLUSTER;
-            objectRequest.title = " My Cluster 2";
+            objectRequest.type = ObjectType.SERVER;
+            objectRequest.title = " My Server";
             objectRequest.cmdbStatus = CmdbStatus.INOPERATION;
             objectId = objectRequest.Create();
 
             //Act: Create the Category
-            categoryRequest.ipv4_address = "1.1.1.2";
-            categoryRequest.description = "Web GUI description";
+            categoryRequest.ipv4_address = "0.0.0.100";
+            categoryRequest.primary = 0;
+            categoryRequest.active = 0;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.description = "IP category test description";
             cateId = IP.Create(objectId, categoryRequest);
 
-          
+            //Assert
+            Assert.IsNotNull(cateId);
+
+            //Act
+            IP.Delete(objectId, cateId);
+
+            //Act: Read the Category
+            categoryRequest.category_id = cateId;
+            list = IP.Read(objectId);
+
+            //Assert
+            foreach (IPResponse[] row in list)
+            {
+                foreach (IPResponse element in row)
+                {
+                    Assert.IsNotNull(element.iPV4Address);
+                    Assert.AreEqual(element.active.title, "No");
+                    Assert.IsNotNull(element.id);
+                }
+            }
+
+            ////Act:Delete the Object
+            objectRequest.Delete(objectId);
+        }
+
+        //Quickpurge
+        [TestMethod]
+        public void QuickpurgeTest()
+        {
+            //Arrange
+            int cateId, objectId;
+            List<IPResponse[]> list = new List<IPResponse[]>();
+            IPRequset categoryRequest = new IPRequset();
+            Client myClient = new Client(URL, APIKEY, LANGUAGE);
+            myClient.Username = "admin";
+            myClient.Password = "admin";
+            Obj objectRequest = new Obj(myClient);
+            IP IP = new IP(myClient);
+            //Act:Create the Object
+            objectRequest.type = ObjectType.SERVER;
+            objectRequest.title = " My Server";
+            objectRequest.cmdbStatus = CmdbStatus.INOPERATION;
+            objectId = objectRequest.Create();
+
+            //Act: Create the Category
+            categoryRequest.ipv4_address = "0.0.0.100";
+            categoryRequest.primary = 0;
+            categoryRequest.active = 0;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.description = "IP category test description";
+            cateId = IP.Create(objectId, categoryRequest);
+
+            //Assert
+            Assert.IsNotNull(cateId);
+
+            //Act
+            IP.Quickpurge(objectId, cateId);
+
+            //Act: Read the Category
+            categoryRequest.category_id = cateId;
+            list = IP.Read(objectId);
+
+            //Assert
+            foreach (IPResponse[] row in list)
+            {
+                foreach (IPResponse element in row)
+                {
+                    Assert.IsNotNull(element.iPV4Address);
+                    Assert.AreEqual(element.active.title, "No");
+                    Assert.IsNotNull(element.id);
+                }
+            }
+
+            ////Act:Delete the Object
+            objectRequest.Delete(objectId);
+        }
+
+        //Update
+        [TestMethod]
+        public void UpdateTest()
+        {
+            //Arrange
+            int cateId, objectId;
+            List<IPResponse[]> list = new List<IPResponse[]>();
+            IPRequset categoryRequest = new IPRequset();
+            Client myClient = new Client(URL, APIKEY, LANGUAGE);
+            myClient.Username = "admin";
+            myClient.Password = "admin";
+            Obj objectRequest = new Obj(myClient);
+            IP IP = new IP(myClient);
+            //Act:Create the Object
+            objectRequest.type = ObjectType.SERVER;
+            objectRequest.title = " My Server";
+            objectRequest.cmdbStatus = CmdbStatus.INOPERATION;
+            objectId = objectRequest.Create();
+
+            //Act: Create the Category
+            categoryRequest.ipv4_address = "0.0.0.100";
+            categoryRequest.primary = 0;
+            categoryRequest.active = 0;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.description = "IP category test description";
+            cateId = IP.Create(objectId, categoryRequest);
+
+            //Act: Update the Category
+            categoryRequest.primary = 1;
+            categoryRequest.active = 1;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.category_id = cateId;
+            IP.Update(objectId, categoryRequest);
+
             //Act:Read the Category
             categoryRequest.category_id = cateId;
             list = IP.Read(objectId);
@@ -245,10 +228,64 @@ namespace UnitTestApi.CMDB.Category.MultiValueCategory
             {
                 foreach (IPResponse element in row)
                 {
-                    Assert.IsNotNull(element.ipv4_address.refTitle);
+                    Assert.IsNotNull(element.iPV4Address);
+                    Assert.AreEqual(element.active.title, "Yes");
+                    Assert.IsNotNull(element.id);
                 }
             }
+
             //Act:Delete the Object
+            objectRequest.Delete(objectId);
+
+        }
+
+        //Read
+        [TestMethod]
+        public void ReadTest()
+        {
+            //Arrange
+            int cateId, objectId;
+            List<IPResponse[]> list = new List<IPResponse[]>();
+            IPRequset categoryRequest = new IPRequset();
+            Client myClient = new Client(URL, APIKEY, LANGUAGE);
+            myClient.Username = "admin";
+            myClient.Password = "admin";
+            Obj objectRequest = new Obj(myClient);
+            IP IP = new IP(myClient);
+            //Act:Create the Object
+            objectRequest.type = ObjectType.SERVER;
+            objectRequest.title = " My Server";
+            objectRequest.cmdbStatus = CmdbStatus.INOPERATION;
+            objectId = objectRequest.Create();
+
+            //Act: Create the Category
+            categoryRequest.ipv4_address = "0.0.0.100";
+            categoryRequest.primary = 0;
+            categoryRequest.active = 0;
+            categoryRequest.domain = "Domain";
+            categoryRequest.net_type = 1;
+            categoryRequest.description = "IP category test description";
+            cateId = IP.Create(objectId, categoryRequest);
+
+            //Assert
+            Assert.IsNotNull(cateId);
+
+            //Act: Read the Category
+            categoryRequest.category_id = cateId;
+            list = IP.Read(objectId);
+
+            //Assert
+            foreach (IPResponse[] row in list)
+            {
+                foreach (IPResponse element in row)
+                {
+                    Assert.IsNotNull(element.iPV4Address);
+                    Assert.AreEqual(element.active.title, "No");
+                    Assert.IsNotNull(element.id);
+                }
+            }
+
+            ////Act:Delete the Object
             objectRequest.Delete(objectId);
         }
     }
