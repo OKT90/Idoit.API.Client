c26633e4efa0be96a3792db4bf3eba12a5141398
Omar Talaat <otalaat@i-doit.com>
2021-10-05 14:46:19 +0200

    Created the class IP request

--- Idoit.API.Client/CMDB/Category/Request/IP.cs
+++ Idoit.API.Client/CMDB/Category/Request/IP.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Idoit.API.Client.CMDB.Category.Request
+﻿namespace Idoit.API.Client.CMDB.Category.Request
 {
    public class IP : IRequest
     {
         public int category_id { get; set; }
+        public string primary_layer3_net { get; set; }
         public string primary_hostaddress { get; set; }
         public string primary_hostname { get; set; }
         public int net_type { get; set; }
@@ -24,14 +21,13 @@ namespace Idoit.API.Client.CMDB.Category.Request
         public string domain { get; set; }
         public int dns_server { get; set; }
         public string dns_server_address { get; set; }
-        public int search_domain { get; set; }
+        public int dns_domain { get; set; }
         public int use_standard_gateway { get; set; }
+        public int assigned_port { get; set; }
         public int assigned_logical_port { get; set; }
         public string all_ips { get; set; }
         public string primary_fqdn { get; set; }
         public string aliases { get; set; }
         public string description { get; set; }
-        
-
-    }
+   }
 }
