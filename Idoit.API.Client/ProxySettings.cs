b171eaef5a01e600db41a7e4950e0048638ea080
Omar Talaat <otalaat@i-doit.com>
2021-10-19 10:09:44 +0200

    Added a new class with the name 'ProxySettings.cs'

---
+++ Idoit.API.Client/ProxySettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Idoit.API.Client
+{
+    public class ProxySettings
+    {
+        public string proxyAddress = "";
+        public string proxyPort = "";
+        public string proxyUserName = "";
+        public string proxyPassword = "";
+    }
+}
